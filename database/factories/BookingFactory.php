478e3762ce7edd4302865cd7735bea42c244d5a6
Miomir Pesic <miomir.pesic@quantox.com>
2019-03-01 14:26:57 +0100

    Made some changes to factory and seeds

--- database/factories/BookingFactory.php
+++ database/factories/BookingFactory.php
@@ -5,12 +5,12 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Booking::class, function (Faker $faker) {
 
-    $start_time = Carbon::now()->addWeeks(rand(1,12))->addDays(rand(1,100));
+    $start_time = Carbon::create()->addWeeks(rand(1,12))->addDays(rand(1,50));
     $end_time = $start_time->addHours(rand(1, 8));
 
     return [
         'car_id' => factory(App\Car::class)->create()->id,
-        'service_id' => factory(App\Service::class)->create()->id,
+        'service_id' => \App\Service::all()->random()->id,
         'start_time' => $start_time,
         'end_time' => $end_time
     ];
--- database/seeds/ServicesTableSeeder.php
+++ database/seeds/ServicesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
-
-use Illuminate\Database\Seeder;
+    
+    use App\Service;
+    use Illuminate\Database\Seeder;
 
 class ServicesTableSeeder extends Seeder
 {
@@ -11,6 +12,67 @@ class ServicesTableSeeder extends Seeder
      */
     public function run()
     {
-        factory('App\Service', 5)->create();
+        Service::create([
+            'name' =>'Veliki servis',
+            'time_required' => '6:00:00',
+            'warranty' => '50000',
+            'cost' => '20000'
+        ]);
+        
+        Service::create([
+            'name' =>'Mali servis',
+            'time_required' => '3:00:00',
+            'warranty' => '1000',
+            'cost' => '20000'
+        ]);
+        
+        Service::create([
+            'name' =>'Zamena amortizera',
+            'time_required' => '1:45:00',
+            'warranty' => '25000',
+            'cost' => '20000'
+        ]);
+        
+        Service::create([
+            'name' =>'Popravka alternatora',
+            'time_required' => '2:30:00',
+            'warranty' => '50000',
+            'cost' => '500'
+        ]);
+        
+        Service::create([
+            'name' =>'Set zupcenja',
+            'time_required' => '1:30:00',
+            'warranty' => '10000',
+            'cost' => '4500'
+        ]);
+        
+        Service::create([
+            'name' =>'Sredjivanje korozije',
+            'time_required' => '1:45:00',
+            'warranty' => '100000',
+            'cost' => '2000'
+        ]);
+        
+        Service::create([
+            'name' =>'Zamena bosch pumpe',
+            'time_required' => '0:50:00',
+            'warranty' => '100000',
+            'cost' => '2000'
+        ]);
+        
+        Service::create([
+            'name' =>'Spustanje glave motora',
+            'time_required' => '3:30:00',
+            'warranty' => '100000',
+            'cost' => '2000'
+        ]);
+    
+        Service::create([
+            'name' =>'Ispravka limarije',
+            'time_required' => '0:55:00',
+            'warranty' => '100000',
+            'cost' => '2000'
+        ]);
     }
 }
